ee77d3e13dbf1c9e65f6d6dee4711af317f3edbf
Conor McGrath <conormcgrath123@gmail.com>
2021-07-25 11:29:51 +0100

    updating website layout & styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { LandingComponent } from './landing/landing.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'portfolio', component: PortfolioComponent },
+  { path: 'about-me', component: AboutMeComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { LandingComponent } from './landing/landing.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PortfolioComponent,
-    LandingComponent
+    LandingComponent,
+    BlogPostComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
