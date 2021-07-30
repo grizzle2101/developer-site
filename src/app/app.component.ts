b9c4d9e4cccb042fbf646cf75a6fac9cb8460ced
Conor Mc Grath <conormcgrath123@gmail.com>
2021-07-30 15:31:11 +0100

    hiding navigation on home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-import {  Router } from '@angular/router';
+import { Router, RouterEvent, NavigationEnd } from '@angular/router';
+import { TestBed } from '@angular/core/testing';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'developer-profile';
- }
+  route: string = '';
+
+  constructor(private router: Router) {
+    router.events.subscribe((x) => {
+      if (x instanceof NavigationEnd) {
+        this.route = x.url;
+      }
+    });
+  }
+}
